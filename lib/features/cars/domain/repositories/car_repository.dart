a71348ec2511aa65cbb98bc347fc0b3659307f26
heba <esmailheba31@gmail.com>
2026-08-20 16:19:55 +0300

    Clean up project by removing legacy car and cart folders after successful migration

--- lib/features/cars/domain/repositories/car_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/car_entity.dart';
-
-abstract class CarRepository {
-  Stream<List<CarEntity>> getCars();
-  Future<void> addCar(CarEntity car);
-  Future<void> updateCar(String carId, CarEntity car);
-  Future<void> deleteCar(String carId);
-}
