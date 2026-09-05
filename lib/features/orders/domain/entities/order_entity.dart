1ca306525ff3471fd0170f284c526410cad882ae
heba <esmailheba31@gmail.com>
2026-09-05 18:45:47 +0300

    Add delivery details to order entity

--- lib/features/orders/domain/entities/order_entity.dart
+++ lib/features/orders/domain/entities/order_entity.dart
@@ -9,6 +9,17 @@ class OrderEntity extends Equatable {
   final String paymentMethod;
   final String paymentStatus;
 
+  final String firstName;
+  final String lastName;
+  final String phone;
+  final String street;
+  final String buildingNumber;
+  final String floorNumber;
+  final String apartmentNumber;
+  final String additionalNotes;
+  final double latitude;
+  final double longitude;
+
   const OrderEntity({
     required this.id,
     required this.userId,
@@ -17,16 +28,36 @@ class OrderEntity extends Equatable {
     required this.orderDate,
     this.paymentMethod = 'cashOnDelivery',
     this.paymentStatus = 'pending',
+    this.firstName = '',
+    this.lastName = '',
+    this.phone = '',
+    this.street = '',
+    this.buildingNumber = '',
+    this.floorNumber = '',
+    this.apartmentNumber = '',
+    this.additionalNotes = '',
+    this.latitude = 0,
+    this.longitude = 0,
   });
 
   @override
   List<Object?> get props => [
-    id,
-    userId,
-    carIds,
-    totalPrice,
-    orderDate,
-    paymentMethod,
-    paymentStatus,
-  ];
+        id,
+        userId,
+        carIds,
+        totalPrice,
+        orderDate,
+        paymentMethod,
+        paymentStatus,
+        firstName,
+        lastName,
+        phone,
+        street,
+        buildingNumber,
+        floorNumber,
+        apartmentNumber,
+        additionalNotes,
+        latitude,
+        longitude,
+      ];
 }
